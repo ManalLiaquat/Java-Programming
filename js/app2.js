f39e61871e6a097815da4cb6ad54e2ad26b730be
ManalLiaquat <m.manalliaquat@gmail.com>
2018-02-06 22:15:53 +0500

    These are my first JavaScript Programs

---
+++ js/app2.js
@@ -0,0 +1,5 @@
+alert("hello this is variable for string | APP Two");
+var fullName = "Manal Liaquat";
+var fatherName = "Liaquat Ali";
+alert(fullName);
+alert(fatherName);
---
+++ js/app6.js
@@ -0,0 +1,9 @@
+/* alert("Concatenating text strings | APP Six");
+var userName = "Manal";
+alert("Thanks, "+userName); */
+/* OR */
+
+alert("Concatenating text strings | APP Six");
+var userName = "Manal";
+var message = "Thanks, ";
+alert(message + userName);
